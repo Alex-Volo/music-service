5a2a39580534f06cc0a8abef9c6e9efb39eecad3
Alex Volo <alex_volo@bk.ru>
2023-06-25 13:44:46 +0900

    Added PlayerInterface component

--- src/MainPage/MusicPlayer/PlayerInterface/PlayerInterface.jsx
+++ src/MainPage/MusicPlayer/PlayerInterface/PlayerInterface.jsx
@@ -1,35 +1,9 @@
 import classes from './PlayerInterface.module.css';
-
+import PlayerControls from './PlayerControls/PlayerControls';
 const PlayerInterface = () => {
   return (
-    <div className="playerInterface">
-      <div className="player__controls">
-        <div className="player__btn-prev">
-          <svg className="player__btn-prev-svg" alt="prev">
-            <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
-          </svg>
-        </div>
-        <div className="player__btn-play _btn">
-          <svg className="player__btn-play-svg" alt="play">
-            <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
-          </svg>
-        </div>
-        <div className="player__btn-next">
-          <svg className="player__btn-next-svg" alt="next">
-            <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
-          </svg>
-        </div>
-        <div className="player__btn-repeat _btn-icon">
-          <svg className="player__btn-repeat-svg" alt="repeat">
-            <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
-          </svg>
-        </div>
-        <div className="player__btn-shuffle _btn-icon">
-          <svg className="player__btn-shuffle-svg" alt="shuffle">
-            <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
-          </svg>
-        </div>
-      </div>
+    <div className={classes.playerInterface}>
+      <PlayerControls />
 
       <div className="playerTrack">
         <div className="track-play__contain">
