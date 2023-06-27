35eb51dc9fbe1f49ea0cf58c408e296faf41e02d
Alex Volo <alex_volo@bk.ru>
2023-06-27 13:29:10 +0900

    Made logick for burger-menu

--- src/MainPage/MainNav/MainNav.jsx
+++ src/MainPage/MainNav/MainNav.jsx
@@ -1,18 +1,23 @@
 import classes from './MainNav.module.css';
 import MainNav__Link from '../MainNav__Link/MainNav__Link';
 import sprite from 'img/icon/sprite.svg';
+import { useState } from 'react';
 
 const MainNav = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const handleClickBurger = () => setIsVisible(!isVisible);
   return (
     <nav className={classes.mainNav}>
-      <svg className={classes.burgerSvg}>
+      <svg onClick={handleClickBurger} className={classes.burgerSvg}>
         <use xlinkHref={`${sprite}#icon-burger`} />
       </svg>
-      <ul className={classes.navList}>
-        <MainNav__Link linkName={'Главное'} />
-        <MainNav__Link linkName={'Мой плейлист'} />
-        <MainNav__Link linkName={'Выйти'} />
-      </ul>
+      <div className={classes.navListContainer}>
+        <ul className={`${classes.navList} ${isVisible && classes.active}`}>
+          <MainNav__Link linkName={'Главное'} />
+          <MainNav__Link linkName={'Мой плейлист'} />
+          <MainNav__Link linkName={'Выйти'} />
+        </ul>
+      </div>
     </nav>
   );
 };
