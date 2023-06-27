f6758175a44c1fc9128c0189885dc22009f95515
Alex Volo <alex_volo@bk.ru>
2023-06-28 06:40:14 +0900

    working on modal window

--- src/MainPage/Tracks__FilterBtn/Tracks__FilterBtn.jsx
+++ src/MainPage/Tracks__FilterBtn/Tracks__FilterBtn.jsx
@@ -1,14 +1,34 @@
 import classes from './Tracks__FilterBtn.module.css';
 import Tracks__FilterModal from 'MainPage/Tracks__FilterModal/Tracks__FilterModal';
+import { fakeState } from 'helpers/fakeState';
+
 const Tracks__FilterBtn = ({
   id,
   isActive,
   title = 'исполнителю',
   makeBtnActive,
 }) => {
+  const getModalData = () => {
+    const getTypeOfData = () => {
+      switch (title) {
+        case 'году выпуска':
+          return 'release_date';
+        case 'жанру':
+          return 'genre';
+        default:
+          return 'author';
+      }
+    };
+    const data = Array.from(
+      new Set(fakeState.map((track) => track[getTypeOfData()]))
+    ).sort();
+    return data;
+  };
   const handleOnClick = () => {
+    console.log(getModalData());
     isActive ? makeBtnActive(0) : makeBtnActive(id);
   };
+
   return (
     <div>
       <div
