e3cca3bbbb50868195107413ff784bed113a070c
Alex Volo <alex_volo@bk.ru>
2023-06-27 14:31:58 +0900

    Made handler for FilterBtn

--- src/MainPage/Tracks__FilterBtn/Tracks__FilterBtn.jsx
+++ src/MainPage/Tracks__FilterBtn/Tracks__FilterBtn.jsx
@@ -1,7 +1,22 @@
 import classes from './Tracks__FilterBtn.module.css';
 
-const Tracks__FilterBtn = ({ title = 'исполнителю' }) => {
-  return <div className={classes.filterBtn}>{title}</div>;
+const Tracks__FilterBtn = ({
+  id,
+  isActive,
+  title = 'исполнителю',
+  makeBtnActive,
+}) => {
+  const handleOnClick = () => {
+    isActive ? makeBtnActive(0) : makeBtnActive(id);
+  };
+  return (
+    <div
+      onClick={handleOnClick}
+      className={`${classes.filterBtn} ${isActive && classes.active}`}
+    >
+      {title}
+    </div>
+  );
 };
 
 export default Tracks__FilterBtn;
