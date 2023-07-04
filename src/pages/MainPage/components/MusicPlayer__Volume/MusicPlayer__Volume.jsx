23ea7a9bc54a527c068e875d4246b047b7a2e6b3
Alex Volo <alex_volo@bk.ru>
2023-07-04 19:13:13 +0900

    MusicPlayer: remaked by Styled-Cmpts

--- src/pages/MainPage/components/MusicPlayer__Volume/MusicPlayer__Volume.jsx
+++ src/pages/MainPage/components/MusicPlayer__Volume/MusicPlayer__Volume.jsx
@@ -1,21 +1,17 @@
-import classes from './MusicPlayer__Volume.module.css';
+import * as S from './styles';
 import sprite from 'assets/img/icon/sprite.svg';
 
 export const MusicPlayer__Volume = () => {
   return (
-    <div className={classes.volumeBlock}>
-      <div className={classes.icon}>
-        <svg className={classes.iconSvg}>
+    <S.volumeBlock>
+      <div>
+        <S.iconSvg>
           <use xlinkHref={`${sprite}#icon-volume`}></use>
-        </svg>
+        </S.iconSvg>
       </div>
-      <div className="volume__progress _btn">
-        <input
-          className="volume__progress-line _btn"
-          type="range"
-          name="range"
-        />
+      <div>
+        <input type="range" name="range" />
       </div>
-    </div>
+    </S.volumeBlock>
   );
 };
