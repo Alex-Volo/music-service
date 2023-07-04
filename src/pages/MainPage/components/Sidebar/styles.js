484ec041252dc2e6c2c373bd2cfe875364f5cbb3
Alex Volo <alex_volo@bk.ru>
2023-07-04 19:43:28 +0900

    Sidebar: remaked by Stld-Cmpts

--- src/pages/MainPage/components/Sidebar/styles.js
+++ src/pages/MainPage/components/Sidebar/styles.js
@@ -1,8 +1,10 @@
-.sideBar {
+import { styled } from 'styled-components';
+
+export const Sidebar = styled.div`
   box-sizing: border-box;
   padding: 240px 0;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
   gap: 30px;
-}
+`;
