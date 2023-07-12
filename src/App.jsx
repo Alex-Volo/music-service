7e833d53ef9f21a1516641560c0a26d42b1871a9
Alex Volo <alex_volo@bk.ru>
2023-07-12 13:28:47 +0900

    Going to previous commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,12 @@ import { setToken } from 'store/UISlice';
 function App() {
   const dispatch = useDispatch();
   dispatch(setToken(Boolean(Cookies.get('token'))));
-
+  const isAllowed = Boolean(useSelector((state) => state.UI.token));
   return (
     <Routes>
       <Route path="/login" element={<EntryForm form="login" />} />
       <Route path="/registration" element={<EntryForm form="registration" />} />
-      <Route
-        element={
-          <ProtectedRoute
-            isAllowed={Boolean(useSelector((state) => state.UI.token))}
-          />
-        }
-      >
+      <Route element={<ProtectedRoute isAllowed={isAllowed} />}>
         <Route path="/" element={<MainPage />} />
       </Route>
     </Routes>
