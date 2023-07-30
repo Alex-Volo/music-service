3b8a7aedd3ff8bda5c416ae6bcd35abda22dda92
Alex Volo <alex_volo@bk.ru>
2023-07-31 05:46:33 +0900

    pause

--- src/helpers/DAL.js
+++ src/helpers/DAL.js
@@ -1,22 +1,70 @@
 // Data Access Layer
 import axios from 'axios';
 const baseURL = 'https://painassasin.online/';
+
 export const fetchAllTracks = () => {
-  return axios
-    .get(baseURL + 'catalog/track/all/')
-    .then((response) => {
-      return response.data;
-    })
+  return axios.get(baseURL + 'catalog/track/all/').then((response) => {
+    return response.data;
+  });
 };
 
 export const fetchAllPlaylists = () => {
   return axios
     .get(baseURL + 'catalog/selection/')
-    .then((response) => response.data)
+    .then((response) => response.data);
 };
 
 export const fetchPlaylist = (id) => {
   return axios
     .get(baseURL + `catalog/selection/${id}`)
-    .then((response) => response.data)
+    .then((response) => response.data);
 };
+
+export const regNewUser = (email, pass, userName) => {
+  return axios
+    .post(baseURL + 'user/signup/', {
+      email: email,
+      password: pass,
+      username: userName,
+
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      return response.data;
+    });
+};
+export const fetchLogin = (email, pass) => {
+  return axios
+    .post(baseURL + 'user/login/', {
+      email: email,
+      password: pass,
+
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      return response.data;
+    });
+};
+
+// export const regNewUser = (email, pass, userName) => {
+//   fetch('https://painassasin.online/user/signup/', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       email: email,
+//       password: pass,
+//       username: userName,
+//     }),
+//     headers: {
+//       // API требует обязательного указания заголовка content-type, так апи понимает что мы посылаем ему json строчку в теле запроса
+//       'content-type': 'application/json',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+// };
