3bc94f52c434722f9778955dff22474095cb9e65
Alex Volo <alex_volo@bk.ru>
2023-10-29 12:31:55 +0900

    refact: move providers to own dir

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import { BrowserRouter } from 'react-router-dom';
-import { UserContext } from 'store/context';
-
+import { AppProvider } from 'providers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <AppProvider>
+    <App />
+  </AppProvider>
 );
