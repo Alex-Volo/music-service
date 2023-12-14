bc67baa39bff5d24bb02782bbbe4cf7f270b3244
Alex Volo <alex_volo@bk.ru>
2023-12-14 15:24:35 +0900

    feat: add search function

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,27 +2,29 @@ import { useState } from 'react';
 import * as S from './styles';
 import { setIsSearching, setListOfFound } from 'store/tracksSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { isIncludes } from 'helpers/helpers';
+
 export const Search = () => {
   const sprite = process.env.PUBLIC_URL + '/assets/img/sprite.svg';
   const [searchValue, setSearchValue] = useState('');
   const dispatch = useDispatch();
   const tracksList = useSelector((state) => state.tracks.list);
 
-  const isIncludes = (str, value) => {
-    const normValue = value.toLowerCase();
-    return str.toLowerCase().includes(normValue);
-  };
+  useEffect(() => {
+    if (searchValue) {
+      dispatch(setIsSearching(true));
 
-  if (searchValue) {
-    dispatch(setIsSearching(true));
-    const listOfFound = tracksList.filter(
-      (track) =>
-        isIncludes(track.name, searchValue) ||
-        isIncludes(track.album, searchValue) ||
-        isIncludes(track.author, searchValue)
-    );
-    dispatch(setListOfFound(listOfFound));
-  } else dispatch(setIsSearching(false));
+      const listOfFound = tracksList.filter(
+        (track) =>
+          isIncludes(track.name, searchValue) ||
+          isIncludes(track.album, searchValue) ||
+          isIncludes(track.author, searchValue)
+      );
+
+      dispatch(setListOfFound(listOfFound));
+    } else dispatch(setIsSearching(false));
+  }, [searchValue]);
 
   return (
     <S.Search>
